93d8fcb33c18e03527e077d8773a595cb1e1d84b
Mimi <mireiag77@gmail>
2023-04-04 14:26:37 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio_Secuencial_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 5;
+            Console.WriteLine($"{num}");
+        }
+    }
+}
